b4e14e7bac9fed717981a6cde8d573f5105f48e1
AntonioFuziy <antoniofuziy@gmail.com>
2022-10-24 14:36:18 -0300

    fix: changed solution names

--- leetcode/final-interviews/solution2.py
+++ leetcode/final-interviews/solution2.py
@@ -1,16 +1,15 @@
 def max_profit(prices: list[int]) -> int:
     max_value = 0
-    i = 0
-    valley = prices[0]
-    peak = prices[0]
-    while i < len(prices) - 1:
-      while i < len(prices) - 1 and prices[i] >= prices[i+1]:
-        i += 1
-      valley = prices[i]
-      while i < len(prices) - 1 and prices[i] <= prices[i+1]:
-        i += 1
-      peak = prices[i]
-      max_value += peak - valley
+    i = 1
+    while i < len(prices):
+      if prices[i] > prices[i-1]:
+        max_value += prices[i] - prices[i-1]
+      i += 1
     return max_value
 
-print(max_profit([7,1,5,3,6,4]))
+print(max_profit([1, 7, 2, 3, 6, 7, 6, 7]))
+
+import matplotlib.pyplot as plt
+
+plt.plot([0,1,2,3,4,5,6,7,8],[1,7,2,3,6,7,6,7,8])
+plt.show()
